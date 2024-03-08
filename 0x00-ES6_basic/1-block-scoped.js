15bd68ce6de95bccdd649fa803a82f9d0e9deb54
Tyjay00 <tyronejoel04@gmail.com>
2024-03-08 18:49:41 +0000

    Block Scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  var task = false;
+  var task2 = true;
 
   if (trueOrFalse) {
-    task = true; // No need to redeclare; just assign a new value
-    task2 = false; // No need to redeclare; just assign a new value
+    let task = true;
+    let task2 = false;
   }
 
   return [task, task2];
